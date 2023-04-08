8319134e86e2e37f97f50a8eaf11af43347156ca
kobiRasing <kobirasing7@gmail.com>
2023-04-08 19:31:39 +0800

    view jobs employee search bar nonfunctional yet

--- overall/employee-side/view-jobs-employee.php
+++ overall/employee-side/view-jobs-employee.php
@@ -16,6 +16,10 @@
             <img src="../css/images/ico.png" alt="Logo" onclick="location.href='main-menu-employee.php';">
         </ul>
     </nav>
+    <form action = 'view-jobs-employee.php?RecordID=' method = 'post'>
+		Search Record: <input type = 'text' name = 'inSearch'/>
+		  <input type = 'submit' name = 'search' value = 'Search' />
+	</form>
 	<?php
 		// open connection to mysql
 		$sqlConnect = mysqli_connect('localhost','root','');
@@ -27,29 +31,78 @@
         $selectDB = mysqli_select_db($sqlConnect,$dbName);
         if(!$selectDB) 
             die("Failed to select the following databaseL: " . $dbName);
-
+        
         if (isset($_GET['RecordID'])) {
             $RecordID = $_GET['RecordID'];
                 // Retrieve job records from job_record_table with matching RecordID
                 $sql = "SELECT JobID, JobDone, DateofJob, JobCost FROM job_record_table WHERE RecordID = '$RecordID' ORDER BY DateOfJob DESC";
                 $result = mysqli_query($sqlConnect, $sql);
 
-                // Display table of job records with delete buttons
-                if (mysqli_num_rows($result) > 0) {
-                    echo "<table><tr><th>Job Done</th><th>Date of Job</th><th>Job Cost</th><th>Delete</th></tr>";
-                    while($row = mysqli_fetch_assoc($result)) {
-                        echo "<tr>";
-                        echo "<td>" . $row["JobDone"] . "</td>";
-                        echo "<td>" . $row["DateofJob"] . "</td>";
-                        echo "<td>" . $row["JobCost"] . "</td><td>
-                        <form method='post' action='delete-job-record.php'>
-                        <input type='hidden' name='JobID' value='".$row["JobID"]."'>
-                        <button type='submit'>Delete</button>
-                        </form></td></tr>";
+                if(isset($_POST['search'])){
+                    $inSearch = $_POST['inSearch'];
+                    echo $inSearch;
+                    if($inSearch == ''){
+                        // Display table of job records with delete buttons
+                        if (mysqli_num_rows($result) > 0) {
+                            echo "<table><tr><th>Job Done</th><th>Date of Job</th><th>Job Cost</th><th>Delete</th></tr>";
+                            while($row = mysqli_fetch_assoc($result)) {
+
+                                echo "<tr>";
+                                echo "<td>" . $row["JobDone"] . "</td>";
+                                echo "<td>" . $row["DateofJob"] . "</td>";
+                                echo "<td>" . $row["JobCost"] . "</td><td>
+                                <form method='post' action='delete-job-record.php'>
+                                <input type='hidden' name='JobID' value='".$row["JobID"]."'>
+                                <button type='submit'>Delete</button>
+                                </form></td></tr>";
+                            }
+                            echo "</table>";
+                        } else {
+                            echo "<table><tr><td>No job records found for this customer.</td></tr></table>";
+                        }
                     }
-                    echo "</table>";
-                } else {
-                    echo "<table><tr><td>No job records found for this customer.</td></tr></table>";
+                    else{
+                        // Display table of job records with delete buttons
+                        if (mysqli_num_rows($result) > 0) {
+                            echo "<table><tr><th>Job Done</th><th>Date of Job</th><th>Job Cost</th><th>Delete</th></tr>";
+                            while($row = mysqli_fetch_assoc($result)) {
+                                if(str_contains(strtolower($row["JobDone"]),strtolower($inSearch)) || 
+								str_contains(strtolower($row["DateofJob"]),strtolower($inSearch)) || 
+								str_contains(strtolower($row["JobCost"]),strtolower($inSearch))){
+                                    echo "<tr>";
+                                    echo "<td>" . $row["JobDone"] . "</td>";
+                                    echo "<td>" . $row["DateofJob"] . "</td>";
+                                    echo "<td>" . $row["JobCost"] . "</td><td>
+                                    <form method='post' action='delete-job-record.php'>
+                                    <input type='hidden' name='JobID' value='".$row["JobID"]."'>
+                                    <button type='submit'>Delete</button>
+                                    </form></td></tr>";
+                                }
+                            }
+                            echo "</table>";
+                        } else {
+                            echo "<table><tr><td>No job records found for this customer.</td></tr></table>";
+                        }
+                    }
+                }
+                else{
+                    // Display table of job records with delete buttons
+                    if (mysqli_num_rows($result) > 0) {
+                        echo "<table><tr><th>Job Done</th><th>Date of Job</th><th>Job Cost</th><th>Delete</th></tr>";
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>" . $row["JobDone"] . "</td>";
+                            echo "<td>" . $row["DateofJob"] . "</td>";
+                            echo "<td>" . $row["JobCost"] . "</td><td>
+                            <form method='post' action='delete-job-record.php'>
+                            <input type='hidden' name='JobID' value='".$row["JobID"]."'>
+                            <button type='submit'>Delete</button>
+                            </form></td></tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "<table><tr><td>No job records found for this customer.</td></tr></table>";
+                    }
                 }
 
                 mysqli_close($sqlConnect);
